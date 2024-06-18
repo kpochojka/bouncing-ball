6692d4f07bd474f0e5e615efd3c63d25b3edf97e
/kp <37598160+kpochojka@users.noreply.github.com>
2024-06-18 15:34:16 +0200

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import { KameleoonUtils } from "@kameleoon/javascript-sdk";
 
 // -- Constants values
-export const siteCode = "2p2rgz4sbp";
+export const siteCode = "l30c7yj7fp";
 export const featureKey = "bouncing_ball_demo";
 export const visitorCode = KameleoonUtils.getVisitorCode("www.example.com");
